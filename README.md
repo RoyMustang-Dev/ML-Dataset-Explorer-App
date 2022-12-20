0a22f77cf3d32d1b34c4f8ac251f9f4130a2959a
Aditya <adityamishra0996@gmail.com>
2022-12-21 03:44:29 +0530

    Finalized the Project and updated the Required files

--- README.md
+++ README.md
@@ -31,3 +31,43 @@ streamlit run app.py
 .
 .
 ### Getting started with the Project!!
+
+1. Importing all the Required Libraries
+&nbsp;
+
+2. Initialized **`main()`** as it is Required by Streamlit
+&nbsp;
+
+3. This implimentation is for both the pre-saved datasets and the uploded datasets, <br/>
+to upload a dataset simply drag n drop it or select from file explorer!!!
+&nbsp;
+
+4. Started implimenting the Required **`Data level-EDA`** techniques 
+&nbsp;
+
+    a. Selecting a File/Dataset <br/>
+    b. Outputting the Selected File <br/>
+    c. Reading the Data <br/>
+    d. Showing the Dataset <br/>
+    e. Showing the Description of the Dataset <br/>
+    f. Showing the Columns <br/>
+    g. Showing DataTypes of the Columns <br/>
+    h. Selective filtering of Columns to Show <br/>
+    i. Vlaue Counts per Selected Column <br/>
+    j. Showing the Shape of Dataset <br/>
+    k. Showing the Null Values by Column 
+&nbsp;
+
+5. Started implimenting the Required **`Visual level-EDA`** techniques
+&nbsp;
+
+    a. Count Plot <br/>
+    b. Seaborn Correlation Plot <br/>
+    c. Pie Chart <br/>
+    d. Customizable Plots - **`"area","bar","line","hist","box","kde"`**
+&nbsp;
+
+6. Added some information about the Project using **`Streamlit's Sidebar`** <br/>
+&nbsp;
+
+# Happy Coding!!!
--- app.py
+++ app.py
@@ -4,13 +4,11 @@ import os
 import streamlit as st
 
 # EDA Packages
-import numpy as np
 import pandas as pd
 
 # Viz Packages
 import matplotlib
-import matplotlib.pyplot as plt
-matplotlib.use('Agg')
+import matplotlib.pyplot
 import seaborn as sns
 
 def main():
@@ -18,113 +16,270 @@ def main():
     st.title("Machine Learning Dataset Explorer")
     st.subheader("Custom Dataset Explorer Application using Streamlit")
 
-    # Selecting a File/Dataset
-    def file_selector(folder_path = './datasets'):
-        filenames = os.listdir(folder_path)
-        selected_file = st.selectbox("Select a file", filenames)
-        return os.path.join(folder_path,selected_file)
-
-    # Outputting the Selected File
-    filename = file_selector()
-    st.info("File Selected {}".format(filename))
-
-    # Reading the Data
-    df = pd.read_csv(filename)
-
-    # Showing the Dataset
-    if st.checkbox("Show Dataset"):
-        number = st.number_input("Number of Rows to View",5,100)
-        st.dataframe(df.head(number))
-
-    st.subheader("Data Level Exploratory Data Analysis(EDA)")
-
-    # Showing the Description of the Dataset
-    if st.checkbox("Description of the Dataset : Some Basic Statistics"):
-        st.write(df.describe().T)
-
-    # Showing the Columns
-    if st.checkbox("Column Names"):
-        st.write(df.columns)
+    uploaded_file = st.file_uploader("Choose a file")
     
-    # Showing DataTypes of the Columns
-    if st.checkbox("Data Types"):
-        st.write(df.dtypes)
+    if uploaded_file is None:
 
-    # Select Columns to Show
-    if st.checkbox("Select Columns to Show"):
-        all_cols = df.columns.to_list()
-        selected_cols = st.multiselect("Select Columns",all_cols)
-        new_df = df[selected_cols]  
-        st.dataframe(new_df)
+        # Selecting a File/Dataset
+        def file_selector(folder_path = './datasets'):
+            filenames = os.listdir(folder_path)
+            selected_file = st.selectbox("Select a file", filenames)
+            return os.path.join(folder_path,selected_file)
 
-    # Vlaue Counts per Selected Column    
-    if st.checkbox("Value Counts"):
-        cols = df.columns.to_list()
-        col = st.selectbox("Select a Column",cols)
-        st.text("The Value Counts of the Selected Columns are as follows :")
-        st.write(df[col].sort_values(ascending=False).value_counts())
+        # Outputting the Selected File
+        filename = file_selector()
+        st.info("File Selected {}".format(filename))
 
-    # Showing the Shape of Dataset
-    if st.checkbox("Shape of the Dataset"):
-        data_dim = st.radio("Show Dimensions by ",("Rows","Columns"))
-        if data_dim == 'Rows':
-            st.text("Number of Rows")
-            st.write(df.shape[0])
-        elif data_dim == 'Columns':
-            st.text("Number of Columns")
-            st.write(df.shape[1])
-        else:
-            st.write(df.shape)
+        # Reading the Data
+        df = pd.read_csv(filename)
 
-    # Showing the Null Values by Column
-    if st.checkbox("Checking For Null Values"):
-        st.write(df.isna().sum())
+        # Showing the Dataset
+        if st.checkbox("Show Dataset"):
+            number = st.number_input("Number of Rows to View",5,100)
+            st.dataframe(df.head(number))
 
-    st.subheader("Data Visualization : Visual Level EDA")
+        st.subheader("Data Level Exploratory Data Analysis(EDA)")
 
-    st.caption('Customizable Plots :sunglasses:')
+        # Showing the Description of the Dataset
+        if st.checkbox("Description of the Dataset : Some Basic Statistics"):
+            st.write(df.describe().T)
+
+        # Showing the Columns
+        if st.checkbox("Column Names"):
+            st.write(df.columns)
     
-    # Creating Customizable Plots
-    all_col_names = df.columns.to_list()
-    type_of_plot = st.selectbox("Select the type of Plot",["area","bar","line","hist","box","kde"])
-    selected_col_names = st.multiselect("Select the Columns to Plot",all_col_names)
+        # Showing DataTypes of the Columns
+        if st.checkbox("Data Types"):
+            st.write(df.dtypes)
 
-    if st.button("Generate Plot"):
-        st.success("Generating the Customized {} Plot for {}".format(type_of_plot,selected_col_names))
+        # Selective filtering of Columns to Show
+        if st.checkbox("Select Columns to Show"):
+            all_cols = df.columns.to_list()
+            selected_cols = st.multiselect("Select Columns",all_cols)
+            new_df = df[selected_cols]  
+            st.dataframe(new_df)
 
-        # Plots by Streamlit
-        if type_of_plot == 'area':
-            cust_data = df[selected_col_names]
-            st.area_chart(cust_data)
+        # Vlaue Counts per Selected Column    
+        if st.checkbox("Value Counts"):
+            cols = df.columns.to_list()
+            col = st.selectbox("Select a Column",cols)
+            st.text("The Value Counts of the Selected Columns are as follows :")
+            st.write(df[col].sort_values(ascending=False).value_counts())
 
-        elif type_of_plot == 'bar':
-            cust_data = df[selected_col_names]
-            st.bar_chart(cust_data)
+        # Showing the Shape of Dataset
+        if st.checkbox("Shape of the Dataset"):
+            data_dim = st.radio("Show Dimensions by ",("Rows","Columns"))
+            if data_dim == 'Rows':
+                st.text("Number of Rows")
+                st.write(df.shape[0])
+            elif data_dim == 'Columns':
+                st.text("Number of Columns")
+                st.write(df.shape[1])
+            else:
+                st.write(df.shape)
 
-        elif type_of_plot == 'line':
-            cust_data = df[selected_col_names]
-            st.line_chart(cust_data)
+        # Showing the Null Values by Column
+        if st.checkbox("Checking For Null Values"):
+            st.write(df.isna().sum())
 
-        # Custom Plot
-        elif type_of_plot:
-            cust_plot = df[selected_col_names].plot(kind= type_of_plot)
-            st.write(cust_plot)
-            st.set_option('deprecation.showPyplotGlobalUse', False)
+        st.subheader("Data Visualization : Visual Level EDA")
+
+        # Count Plots
+
+        if st.checkbox("Plot of Value Counts"):
+            st.text("Value Counts by Target")
+            all_col_names = df.columns.to_list()
+            primary_col = st.selectbox("Primary Column to GroupBy", all_col_names)
+            selected_col_names = st.multiselect("Select Columns", all_col_names)
+            if st.button("Plot"):
+                st.text("Generate Plot")
+                if selected_col_names:
+                    vc_plot = df.groupby(primary_col)[selected_col_names].count()
+                else:
+                    vc_plot = df.iloc[:,-1].value_counts()
+                st.write(vc_plot.plot(kind="bar"))
+                st.pyplot()
+
+        # Seaborn Plots
+
+        if st.checkbox("Correlation Plot[using Seaborn]"):
+            st.write(sns.heatmap(df.corr(), annot=True))
             st.pyplot()
 
+        # Pie Chart
+
+        if st.checkbox("Pie Chart"):
+            all_col_names = df.columns.to_list()
+            if st.button("Generate Pie Chart"):
+                st.success("Generating Pie Chart")
+                st.write(df.iloc[:,-1].value_counts().plot.pie(autopct = "%1.1f%%"))
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot()
+
+        st.caption('Customizable Plots :sunglasses:')
     
+        # Creating Customizable Plots
+        all_col_names = df.columns.to_list()
+        type_of_plot = st.selectbox("Select the type of Plot",["area","bar","line","hist","box","kde"])
+        selected_col_names = st.multiselect("Select the Columns to Plot",all_col_names)
+
+        if st.button("Generate Plot"):
+            st.success("Generating the Customized {} Plot for {}".format(type_of_plot,selected_col_names))
+
+            # Plots by Streamlit
+            if type_of_plot == 'area':
+                cust_data = df[selected_col_names]
+                st.area_chart(cust_data)
+
+            elif type_of_plot == 'bar':
+                cust_data = df[selected_col_names]
+                st.bar_chart(cust_data)
+
+            elif type_of_plot == 'line':
+                cust_data = df[selected_col_names]
+                st.line_chart(cust_data)
+
+            # Custom Plot
+            elif type_of_plot:
+                cust_plot = df[selected_col_names].plot(kind= type_of_plot)
+                st.write(cust_plot)
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot()
+
+    else:
+
+        # Outputting the Selected File
+        st.write("filename:", uploaded_file.name)
+
+        # Reading the Data
+        df_uploaded = pd.read_csv(uploaded_file)
+
+        # Showing the Dataset
+        if st.checkbox("Show Dataset"):
+            number = st.number_input("Number of Rows to View",5,100)
+            st.dataframe(df_uploaded.head(number))
+
+        st.subheader("Data Level Exploratory Data Analysis(EDA)")
+
+        # Showing the Description of the Dataset
+        if st.checkbox("Description of the Dataset : Some Basic Statistics"):
+            st.write(df_uploaded.describe().T)
+
+        # Showing the Columns
+        if st.checkbox("Column Names"):
+            st.write(df_uploaded.columns)
+    
+        # Showing DataTypes of the Columns
+        if st.checkbox("Data Types"):
+            st.write(df_uploaded.dtypes)
+
+        # Select Columns to Show
+        if st.checkbox("Select Columns to Show"):
+            all_cols = df_uploaded.columns.to_list()
+            selected_cols = st.multiselect("Select Columns",all_cols)
+            new_df = df_uploaded[selected_cols]  
+            st.dataframe(new_df)
+
+        # Vlaue Counts per Selected Column    
+        if st.checkbox("Value Counts"):
+            cols = df_uploaded.columns.to_list()
+            col = st.selectbox("Select a Column",cols)
+            st.text("The Value Counts of the Selected Columns are as follows :")
+            st.write(df_uploaded[col].sort_values(ascending=False).value_counts())
+
+        # Showing the Shape of Dataset
+        if st.checkbox("Shape of the Dataset"):
+            st.write(df_uploaded.shape)
+            data_dim = st.radio("Show Dimensions by ",("Rows","Columns"))
+            if data_dim == 'Rows':
+                st.text("Number of Rows")
+                st.write(df_uploaded.shape[0])
+            elif data_dim == 'Columns':
+                st.text("Number of Columns")
+                st.write(df_uploaded.shape[1])
+            else:
+                st.write(df_uploaded.shape)
+
+        # Showing the Null Values by Column
+        if st.checkbox("Checking For Null Values"):
+            st.write(df_uploaded.isna().sum())
+
+        st.subheader("Data Visualization : Visual Level EDA")
+
+        # Count Plots
+
+        if st.checkbox("Plot of Value Counts"):
+            st.text("Value Counts by Target")
+            all_col_names = df_uploaded.columns.to_list()
+            primary_col = st.selectbox("Primary Column to GroupBy", all_col_names)
+            selected_col_names = st.multiselect("Select Columns", all_col_names)
+            if st.button("Plot"):
+                st.text("Generate Plot")
+                if selected_col_names:
+                    vc_plot = df_uploaded.groupby(primary_col)[selected_col_names].count()
+                else:
+                    vc_plot = df_uploaded.iloc[:,-1].value_counts()
+                st.write(vc_plot.plot(kind="bar"))
+                st.pyplot()
+
+        # Seaborn Plots
+
+        if st.checkbox("Correlation Plot[using Seaborn]"):
+            st.write(sns.heatmap(df_uploaded.corr(), annot=True))
+            st.pyplot()
+
+        # Pie Chart
+
+        if st.checkbox("Pie Chart"):
+            all_col_names = df_uploaded.columns.to_list()
+            if st.button("Generate Pie Chart"):
+                st.success("Generating Pie Chart")
+                st.write(df_uploaded.iloc[:,-1].value_counts().plot.pie(autopct = "%1.1f%%"))
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot()
+
+
+        st.caption('Customizable Plots :sunglasses:')
+    
+        # Creating Customizable Plots
+        all_col_names = df_uploaded.columns.to_list()
+        type_of_plot = st.selectbox("Select the type of Plot",["area","bar","line","hist","box","kde"])
+        selected_col_names = st.multiselect("Select the Columns to Plot",all_col_names)
+
+        if st.button("Generate Plot"):
+            st.success("Generating the Customized {} Plot for {}".format(type_of_plot,selected_col_names))
+
+            # Plots by Streamlit
+            if type_of_plot == 'area':
+                cust_data = df_uploaded[selected_col_names]
+                st.area_chart(cust_data)
+
+            elif type_of_plot == 'bar':
+                cust_data = df_uploaded[selected_col_names]
+                st.bar_chart(cust_data)
+
+            elif type_of_plot == 'line':
+                cust_data = df_uploaded[selected_col_names]
+                st.line_chart(cust_data)
+
+            # Custom Plot
+            elif type_of_plot:
+                cust_plot = df_uploaded[selected_col_names].plot(kind= type_of_plot)
+                st.write(cust_plot)
+                st.set_option('deprecation.showPyplotGlobalUse', False)
+                st.pyplot()
 
     # Information About the Application
     st.sidebar.header("About App")
-    st.sidebar.info("A Simple EDA App for Exploring Common ML Dataset")
+    st.sidebar.info("A Simple Exploratory Data Analysis(EDA) App for Exploring Common Machine Learning Datasets")
 
     st.sidebar.header("Get Datasets")
-    st.sidebar.markdown("[Common ML Dataset Repo]("")")
+    st.sidebar.markdown("[Common ML Dataset Repo](https://github.com/RoyMustang-Dev/ML-Dataset-Explorer-App/tree/main/datasets)")
 
     st.sidebar.header("About")
-    st.sidebar.info("Jesus Saves@JCharisTech")
+    st.sidebar.info("Aditya Mishra")
     st.sidebar.text("Built with Streamlit")
-    st.sidebar.text("Maintained by Jesse JCharis")
+    st.sidebar.text("Maintained by Aditya Mishra")
 
     if st.button("Thanks"):
 	    st.balloons()
